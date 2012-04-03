7c8eb07f125cea4fcb9a30c0c549d9fe392f8e74
Fernando Barillas <fbis251@gmail.com>
2012-04-03 01:32:33 -0700

    Added gitignore Moved TB to /data/app Added Launcher Pro Added update.sh

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+input_file="update.zip"
+remote_dir="/sdcard"
+remote_file="update.zip"
+
+if [ "$1" == "backup" ]; then
+  adb push tools/data-backup /tmp/
+  adb push tools/data-restore /tmp/
+  adb push system/xbin/busybox /tmp/
+  adb shell 'chmod +x /tmp/*'
+  exit
+fi
+
+# Main script
+echo "Creating zip file"
+zip --quiet -r -9 $input_file data META-INF system tools updates
+
+echo "Transferring installer"
+adb push $input_file $remote_dir/$remote_file
+
+if [ "$?" == "0" ]; then
+  echo ""
+  echo "Installer transfered!"
+fi
